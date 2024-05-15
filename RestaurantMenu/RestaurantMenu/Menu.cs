36d26eb1d18728995ffd25bf09da251b5ac8be54
Telly Pippa <telly@Tellys-PC.local>
2024-05-15 14:36:24 -0500

    added menu and menuitem classes

---
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantMenu;
+
+public class Menu
+{
+ private List<MenuItem> MenuItems { get; set; } = [];
+
+ private DateTime LastUpdated { get; }
+
+ public Menu(List<MenuItem> menuItems, DateTime lastUpdated)
+ {
+    MenuItems = menuItems;
+    LastUpdated = lastUpdated;
+ }
+}
---
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿namespace RestaurantMenu;
+
+public class MenuItem
+{
+ private double Price { get; set; }
+ private string Description { get; set; }
+ private string Category { get; set; } // do validation for setter: APP, MAIN COURSE, DESSERT
+ private bool IsNew { get; set; }
+
+ public MenuItem(double price, string description, string category, bool isNew)
+ {
+    Price = price;
+    Description = description;
+    Category = category;
+    IsNew = isNew;
+ }
+}
